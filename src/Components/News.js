ccb31a46891d70c01eb0707a9e0cad2b0c1b9691
Omkar-Narvekar <omkarnarvekar96@gmail.com>
2023-09-16 20:27:45 +0530

    new commit
    add author and source

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
+import PropTypes from 'prop-types'
 
 export class News extends Component {
+  static defaultProps = {
+    country : "in",
+    pageSize: 9,
+    category: "general"
+  }
+
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string
+  }
+
 
 
   articles =  []
@@ -19,7 +32,7 @@ export class News extends Component {
   }
 
    async componentDidMount(){
-    let url=`https://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=5c8a2a44925c4529a0580680c1fd4726&page=1&pageSize=${this.props.pageSize}`;
+    let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5c8a2a44925c4529a0580680c1fd4726&page=1&pageSize=${this.props.pageSize}`;
     this.setState({loading:true})
     let data = await fetch(url);
     let parseddata = await data.json();
@@ -31,7 +44,7 @@ export class News extends Component {
     }
 
     handleprev = async ()=>{
-      let url=`https://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=5c8a2a44925c4529a0580680c1fd4726&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+      let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5c8a2a44925c4529a0580680c1fd4726&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
       this.setState({loading:true}) 
       let data = await fetch(url);
       let parseddata = await data.json();
@@ -46,7 +59,7 @@ export class News extends Component {
       console.log();
 
       if(!(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))){
-      let url=`https://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=5c8a2a44925c4529a0580680c1fd4726&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+      let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5c8a2a44925c4529a0580680c1fd4726&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
       this.setState({loading:true});
       let data = await fetch(url);
       let parseddata = await data.json();
@@ -60,21 +73,26 @@ export class News extends Component {
 
 
   render() {
+
+    const totalPages = Math.ceil(this.state.totalResults / this.props.pageSize);
+
     return (
       <div className='container my-3'>
-        <h1 className='text-center'>News Alert Top Headlines</h1>
+        <h1 className='text-center' style={{margin: '35px 0px'}}>News Alert Top Headlines</h1>
         {this.state.loading && <Spinner/>}
         <div className='row'>
           {!this.state.loading && this.state.articles.map((element)=>{
            return <div className='col-md-4' key={element.url}>
-            <NewsItem Title={element.title ? element.title.slice(0, 45) : ""} Description={element.description ? element.description.slice(0, 88) : ""} newsUrl={element.url} imgUrl={element.urlToImage}/>
+            <NewsItem Title={element.title ? element.title.slice(0, 45) : ""} Description={element.description ? element.description.slice(0, 88) : ""} newsUrl={element.url} imgUrl={element.urlToImage} author={element.author} date={element.publishedAt} />
             </div>
           })}
         </div>
         <div className='container d-flex justify-content-around'>
         <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handleprev}>&larr; Previous</button>
-        <button type="button" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize
-        )} className="btn btn-dark" onClick={this.handlenext}>Next &rarr;</button>
+          <div className="page-box" style={{border: '1px solid black', borderRadius: '4px', color: "white", backgroundColor: 'black', padding: '8px 16px'}}>
+            <label>{this.state.page}/{totalPages}</label>
+          </div>
+        <button type="button" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} className="btn btn-dark" onClick={this.handlenext}>Next &rarr;</button>
         </div>
       </div>
     )
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,22 +1,61 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-   let {Title, Description, imgUrl, newsUrl} = this.props;
-    
-   return (
-      <div className='my-3'>
-        <div className="card" style={{width: "18rem"}}>
-        <img src={!imgUrl?"https://www.shutterstock.com/shutterstock/photos/1443999785/display_1500/stock-vector-bollywood-word-on-gradient-background-indian-cinema-poster-with-text-and-spot-light-indian-1443999785.jpg":imgUrl} className="card-img-top" alt={Title}/>
-        <div className="card-body">
+    let { Title, Description, imgUrl, newsUrl, author, date } = this.props;
+
+    const eventDate = new Date(date);
+
+    // Convert the date and time to Indian Standard Time (IST)
+    const istDateOptions = {
+      timeZone: "Asia/Kolkata",
+      hour12: true,
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    };
+    const istDateString = eventDate.toLocaleString("en-IN", istDateOptions);
+
+    return (
+      <div className="my-3">
+        <div className="card">
+          <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+            99+
+            <span class="visually-hidden">unread messages</span>
+          </span>
+          <img
+            src={
+              !imgUrl
+                ? "https://www.shutterstock.com/shutterstock/photos/1443999785/display_1500/stock-vector-bollywood-word-on-gradient-background-indian-cinema-poster-with-text-and-spot-light-indian-1443999785.jpg"
+                : imgUrl
+            }
+            className="card-img-top"
+            alt={Title}
+          />
+          <div className="card-body">
             <h5 className="card-title">{Title}...</h5>
             <p className="card-text">{Description}...</p>
-            <a href={newsUrl} target='_blank' rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
+            <p className="card-text">
+              <small class="text-body-secondary">
+                By {author} on {istDateString} IST
+              </small>
+            </p>
+            <a
+              href={newsUrl}
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-sm btn-dark"
+            >
+              Read More
+            </a>
+          </div>
         </div>
-        </div>
-    </div>
-    )
+      </div>
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
